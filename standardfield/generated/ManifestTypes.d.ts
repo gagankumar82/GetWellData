e11906fbf80f27ab59995d162d4deea04426f6e4
gagankumar82 <59870938+gagankumar82@users.noreply.github.com>
2025-06-21 16:05:17 +0530

    Add rpc_wellapi typings and fix webAPI execute

---
+++ standardfield/generated/ManifestTypes.d.ts
@@ -0,0 +1,11 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    rpc_wellapi: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    rpc_wellapi?: string;
+}
